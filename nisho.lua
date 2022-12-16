437394e225e6b362cf1f4b6b0dca861301b862df
sonocircuit <49308219+sonocircuit@users.noreply.github.com>
2022-12-16 15:16:16 +0100

    Update nisho.lua

--- nisho.lua
+++ nisho.lua
@@ -5,16 +5,16 @@
 -- keyboard and
 -- pattern recorder
 --
--- 0.1.2 @sonocircuit
--- llllllll.co/t/nisha
+-- 0.1.4 @sonocircuit
+-- llllllll.co/t/nisho
 --
 --
 -- for docs go to:
 -- >> github.com/sonocircuits
---    /nisha
+--    /nisho
 --
 -- or smb into:
--- >> code/nisha/docs
+-- >> code/nisho/docs
 --
 --
 
@@ -32,7 +32,8 @@ g = grid.connect()
 -------- variables --------
 
 -- locals
-local load_pset = false
+local load_pset = true
+local pageNum = 1
 local int_focus = 1
 local key_focus = 1
 local alt = false
@@ -40,6 +41,8 @@ local shift = false
 local key_link = true
 local mute_int = false
 local mute_key = false
+local transpose = false
+local transpose_value = 0
 local set_pattern = false
 local overdub = false
 local quantize = false
@@ -75,11 +78,17 @@ notes.oct_key = 0
 notes.last = 1
 notes.home = 1
 notes.played = 60
+notes.root = {}
+notes.key = {}
+notes.ref = {}
+for i = 1, 12 do
+  notes.root[i] = nil
+  notes.key[i] = nil
+end
 
 voice = {}
 for i = 1, 2 do -- 2 voices
   voice[i] = {}
-  --voice[i].note_num = 60
   voice[i].output = 1
   voice[i].mute = false
 end
@@ -106,7 +115,7 @@ end
 
 -------- scales --------
 
-scale_notes = {}
+local scale_notes = {}
 
 function build_scale()
   local root = params:get("root_note") % 12 + 24
@@ -119,6 +128,23 @@ end
 
 function set_root()
   notes.home = tab.key(scale_notes, params:get("root_note"))
+  for i = 1, 12 do
+    if i == params:get("root_note") % 12 + 1 then
+      notes.root[i] = true
+    else
+      notes.root[i] = false
+    end
+  end
+end
+
+function set_keys() -- check if note is in the selected scale
+  for i = 1, 12 do
+    if lookup(i + 23) then
+      notes.key[i] = true
+    else
+      notes.key[i] = false
+    end
+  end
 end
 
 function lookup(note) -- check if note is in the selected scale
@@ -132,13 +158,19 @@ function lookup(note) -- check if note is in the selected scale
    return false
 end
 
+function set_scale()
+  build_scale()
+  set_root()
+  set_keys()
+end
+
 -------- voice settings --------
 
 function set_voice_output()
   local count = 0
   for i = 1, 2 do
     voice[i].output = params:get("voice_out"..i)
-    if params:get("voice_out"..i) == 5 then
+    if voice[i].output == 5 then
       count = count + 1
     end
   end
@@ -160,11 +192,11 @@ function build_midi_device_list()
   end
 end
 
-function midi.add() -- MIDI register callback
+function midi.add()
   build_midi_device_list()
 end
 
-function midi.remove() -- MIDI remove callback
+function midi.remove()
   clock.run(
     function()
       clock.sleep(0.2)
@@ -203,7 +235,8 @@ end
 
 local eINT = 1
 local eKEY = 2
-local ePATTERN = 3
+local eTRANSPOSE = 3
+local ePATTERN = 4
 local quant_event = {}
 
 function event_record(e)
@@ -259,16 +292,26 @@ end
 -- exec function
 function event_exec(e)
   if e.t == eINT then
-    if not mute_int then
-      local note_num = scale_notes[e.note]
-      play_voice(e.i, note_num)
-      notes.played = note_num
+    local idx = e.note
+    if transpose then
+      idx = util.clamp(e.note + transpose_value, 1, #scale_notes)
     end
+    local note_num = scale_notes[idx]
+    play_voice(e.i, note_num)
+    notes.played = note_num
   elseif e.t == eKEY then
-    local note_num = scale_notes[e.note]
-    if not mute_key then
-      play_voice(e.i, note_num)
-      notes.played = note_num
+    local idx = e.note
+    if transpose then
+      idx = util.clamp(e.note + transpose_value, 1, #scale_notes)
+    end
+    local note_num = scale_notes[idx]
+    play_voice(e.i, note_num)
+    notes.played = note_num
+  elseif e.t == eTRANSPOSE then
+    local home_note = tab.key(scale_notes, params:get("root_note"))
+    transpose_value = util.clamp(transpose_value + e.interval, -home_note + 1, #scale_notes - home_note)
+    if e.interval == 0 then
+      transpose_value = 0
     end
   elseif e.t == ePATTERN then
     if e.action == "stop" then
@@ -358,15 +401,15 @@ function init()
   -- global params
   params:add_separator("global_settings", "global")
   params:add_option("scale", "scale", scale_names, 2)
-  params:set_action("scale", function() build_scale() set_root() end)
+  params:set_action("scale", function() set_scale() dirtygrid = true end)
 
   params:add_number("root_note", "root note", 24, 84, 60, function(param) return mu.note_num_to_name(param:get(), true) end)
-  params:set_action("root_note", function() build_scale() set_root() end)
+  params:set_action("root_note", function() set_scale() dirtygrid = true end)
 
   params:add_option("pattern_length", "pattern length", options.pattern_length , 2)
   params:set_action("pattern_length", function(idx) set_pattern_len() end)
 
-  params:add_option("pattern_quant", "pattern quantization", options.pattern_quant, 8)
+  params:add_option("pattern_quant", "quantization", options.pattern_quant, 8)
   params:set_action("pattern_quant", function(idx) q_rate = options.quant_value[idx] * 4 end)
 
   -- voice params
@@ -411,8 +454,10 @@ function init()
 
   params:add_separator("sound", "sound")
   -- engine params
-  params:add_group("thebangs", "thebangs", 8)
+  params:add_group("thebangs", "thebangs", 9)
   thebangs.synth_params()
+  --thebangs.voice_params()
+
   -- delay params
   params:add_group("delay", "delay", 8)
   halfsync.init()
@@ -533,15 +578,13 @@ function init()
   fastpulse = metro.init(ledpulse_fast, 0.1, -1)
   fastpulse:start()
 
+  -- pattern clocks
+  for i = 1, 8 do
+    pattern[i]:init_clock()
+  end
+
   -- hardware callbacks
   grid.add = drawgrid_connect
-
-  norns.crow.add = function()
-    --norns.crow.init() -- this initalized crow and norns part of crow
-    --if params:get("jf_toggle") == 2 then
-      --crow.ii.jf.mode(1)
-    --end
-  end
 
 end
 
@@ -591,18 +634,23 @@ function enc(n, d)
   elseif n == 2 then
     if shift then
       params:delta("scale", d)
+    elseif set_pattern then
+      params:delta("pattern_length", d)
+      dirtygrid = true
     else
       --
     end
   elseif n == 3 then
     if shift then
       params:delta("root_note", d)
+    elseif set_pattern then
+      params:delta("pattern_quant", d)
+      dirtygrid = true
     else
       --
     end
   end
   dirtyscreen = true
-  dirtygrid = true
 end
 
 function key(n, z)
@@ -610,7 +658,10 @@ function key(n, z)
     shift = z == 1 and true or false
   end
   if n == 2 and z == 1 then
-    --
+    transpose = not transpose
+    if not transpose then
+      transpose_value = 0
+    end
   elseif n == 3 and z == 1 then
     --
   end
@@ -622,6 +673,7 @@ function redraw()
   screen.clear()
   if set_pattern then
     screen.level(15)
+    screen.font_size(8)
     screen.move(64, 18)
     screen.text_center("pattern length")
     screen.move(64, 44)
@@ -634,16 +686,26 @@ function redraw()
   else
     local note_count = #scale_intervals[params:get("scale")] - 1
     for i = 1, note_count do
+      screen.font_size(8)
       screen.level((notes.played - scale_notes[i]) % 12 == 0 and 15 or 2)
       screen.move(i * 14 + (-7 * note_count + 56), 36)
       screen.text_center(mu.note_num_to_name(scale_notes[i], false))
     end
     if shift then
       screen.level(8)
+      screen.font_size(8)
       screen.move(8, 58)
       screen.text(params:string("scale"))
       screen.move(110, 58)
       screen.text(params:string("root_note"))
+    end
+    if transpose then
+      local home_note = tab.key(scale_notes, params:get("root_note"))
+      local semitones = scale_notes[home_note + transpose_value] - params:get("root_note")
+      screen.level(6)
+      screen.font_size(16)
+      screen.move(64, 18)
+      screen.text_center("transpose "..semitones)
     end
   end
   screen.update()
@@ -732,36 +794,49 @@ function g.key(x, y, z)
         end
       end
     elseif y == 3 then
-      -- set pattern length
+      -- set pattern loop
       if x > 4 and x < 13 and set_pattern then
-        local val = x - 4
-        params:set("pattern_length", val)
-        dirtyscreen = true
+        local i = x - 4
+        pattern[i].loop = not pattern[i].loop
       -- play home note
       elseif x > 7 and x < 10 and not set_pattern then
-        local e = {t = eINT, i = int_focus, note = tab.key(scale_notes, params:get("root_note"))} event(e)
-        notes.last = e.note
+        local home_note = tab.key(scale_notes, params:get("root_note"))
+        if not transpose then
+          if not mute_int then
+            local e = {t = eINT, i = int_focus, note = home_note} event(e)
+          end
+        else
+          local e = {t = eTRANSPOSE, interval = 0} event(e)
+        end
+        notes.last = home_note
       end
     elseif y == 4 then
       -- interval octave up
       if x == 1 then
         notes.oct_int = util.clamp(notes.oct_int + 1, -3, 3)
-      -- set pattern quantization
-      elseif x > 4 and x < 13 and set_pattern then
-        local val = x - 4
-        params:set("pattern_quant", val)
-        dirtyscreen = true
       -- interval decrease
       elseif x > 3 and x < 8 and not set_pattern then
         local interval = x - 8
         local new_note = util.clamp(notes.last + interval, 1, #scale_notes)
-        local e = {t = eINT, i = int_focus, note = new_note} event(e)
+        if not transpose then
+          if not mute_int then
+            local e = {t = eINT, i = int_focus, note = new_note} event(e)
+          end
+        else
+          local e = {t = eTRANSPOSE, interval = interval} event(e)
+        end
         notes.last = new_note
       -- interval increase
       elseif x > 9 and x < 14 and not set_pattern then
         local interval = x - 9
         local new_note = util.clamp(notes.last + interval, 1, #scale_notes)
-        local e = {t = eINT, i = int_focus, note = new_note} event(e)
+        if not transpose then
+          if not mute_int then
+            local e = {t = eINT, i = int_focus, note = new_note} event(e)
+          end
+        else
+          local e = {t = eTRANSPOSE, interval = interval} event(e)
+        end
         notes.last = new_note
       -- toggle quantization
       elseif x == 16 then
@@ -780,9 +855,28 @@ function g.key(x, y, z)
       -- interval octave down
       if x == 1 then
         notes.oct_int = util.clamp(notes.oct_int - 1, -3, 3)
+      -- set pattern length
+      elseif x > 4 and x < 13 and set_pattern then
+          local val = x - 4
+          params:set("pattern_length", val)
+          dirtyscreen = true
       -- play last note
       elseif x > 7 and x < 10 and not set_pattern then
-        local e = {t = eINT, i = int_focus, note = notes.last} event(e)
+        if not transpose then
+          if not mute_int then
+            local e = {t = eINT, i = int_focus, note = notes.last} event(e)
+          end
+        else
+          local octave = (#scale_intervals[params:get("scale")] - 1) * (x - 8 == 0 and -1 or 1)
+          local e = {t = eTRANSPOSE, interval = octave} event(e)
+        end
+      end
+    elseif y == 6 then
+      -- set pattern quantization
+      if x > 4 and x < 13 and set_pattern then
+        local val = x - 4
+        params:set("pattern_quant", val)
+        dirtyscreen = true
       end
     -- key octave up
     elseif y == 7 and x == 1 then
@@ -793,9 +887,11 @@ function g.key(x, y, z)
     -- play keys
     elseif (y == 7 or y == 8) and x > 2 and x < 15 then
       local new_note = (60 + x - 3) + (notes.oct_key + (2 - (y - 6))) * 12
-      if tab.key(scale_notes, new_note) ~= nil then
-        local e = {t = eKEY, i = key_focus, note = tab.key(scale_notes, new_note)} event(e)
-        if key_link then
+      if tab.key(scale_notes, new_note) ~= nil and not set_pattern then
+        if not mute_key then
+          local e = {t = eKEY, i = key_focus, note = tab.key(scale_notes, new_note)} event(e)
+        end
+        if key_link and not transpose then
           local octave = #scale_intervals[params:get("scale")] - 1
           notes.last = tab.key(scale_notes, new_note) + octave * notes.oct_int
         end
@@ -824,10 +920,12 @@ function gridredraw()
   g:led(16, 4, quantize and (flash_bar and 15 or (flash_beat and 10 or 7)) or 3) -- Q flash
   for i = 1, 8 do
     if set_pattern then
-      g:led(i + 4, 1, pattern[i].synced and 8 or 2)
-      g:led(i + 4, 2, pattern[i].count_in_num == 1 and 2 or 4)
-      g:led(i + 4, 3, params:get("pattern_length") == i and 15 or 4)
-      g:led(i + 4, 4, params:get("pattern_quant") == i and 15 or 2)
+      g:led(i + 4, 1, pattern[i].synced and 10 or 4)
+      g:led(i + 4, 2, pattern[i].count_in_num == 4 and 6 or 2)
+      g:led(i + 4, 3, pattern[i].loop and 0 or 4)
+      g:led(i + 4, 5, params:get("pattern_length") == i and 15 or 8)
+      g:led(i + 4, 6, params:get("pattern_quant") == i and 15 or 4)
+      g:led(i + 4, 8, 2) -- TODO: pset load slots
     else
       if pattern[i].rec == 1 then
         g:led(i + 4, 1, 15)
@@ -836,9 +934,9 @@ function gridredraw()
       elseif pattern[i].play == 1 then
         g:led(i + 4, 1, pattern[i].flash and 15 or 12)
       elseif pattern[i].count > 0 then
-        g:led(i + 4, 1, 8)
+        g:led(i + 4, 1, 7)
       else
-        g:led(i + 4, 1, 4)
+        g:led(i + 4, 1, 3)
       end
       -- mute interval
       for i = 8, 9 do
@@ -879,20 +977,12 @@ function gridredraw()
   g:led(1, 7, 8 + notes.oct_key * 2)
   g:led(1, 8, 8 - notes.oct_key * 2)
   -- keyboard
-  local root = params:get("root_note") % 12 + 24
-  for i = 7, 8 do
-    g:led(3, i, root == 24 and 15 or (lookup(24) and 10 or 0))  -- C
-    g:led(4, i, root == 25 and 15 or (lookup(25) and 10 or 0)) -- C#
-    g:led(5, i, root == 26 and 15 or (lookup(26) and 10 or 0)) -- D
-    g:led(6, i, root == 27 and 15 or (lookup(27) and 10 or 0)) -- D#
-    g:led(7, i, root == 28 and 15 or (lookup(28) and 10 or 0)) -- E
-    g:led(8, i, root == 29 and 15 or (lookup(29) and 10 or 0)) -- F
-    g:led(9, i, root == 30 and 15 or (lookup(30) and 10 or 0)) -- F#
-    g:led(10, i, root == 31 and 15 or (lookup(31) and 10 or 0)) -- G
-    g:led(11, i, root == 32 and 15 or (lookup(32) and 10 or 0)) -- G#
-    g:led(12, i, root == 33 and 15 or (lookup(33) and 10 or 0)) -- A
-    g:led(13, i, root == 34 and 15 or (lookup(34) and 10 or 0)) -- A#
-    g:led(14, i, root == 35 and 15 or (lookup(35) and 10 or 0)) -- B
+  for i = 1, 12 do
+    for j = 1, 2 do
+      if not set_pattern then
+        g:led(i + 2, j + 6, notes.root[i] and 15 or (notes.key[i] and 10 or 0))
+      end
+    end
   end
   -- key link
   g:led(16, 7, key_link and 10 or 4)
@@ -968,6 +1058,12 @@ function build_menu()
   dirtyscreen = true
 end
 
+function page_redraw(page)
+  if pageNum == page then
+    dirtyscreen = true
+  end
+end
+
 function drawgrid_connect()
   dirtygrid = true
   gridredraw()
@@ -978,6 +1074,7 @@ function cleanup()
   crow.ii.jf.mode(0)
   for i = 1, 8 do
     pattern[i]:stop()
+    pattern[i].sync_clock = nil
     pattern[i] = nil
   end
 end
