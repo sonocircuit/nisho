c180ee20f4d1ddb3a7a6f81a808db15a603efd18
sonocircuit <49308219+sonocircuit@users.noreply.github.com>
2024-12-01 02:32:14 +0100

    reflection whizz

--- lib/nishos_reflection.lua
+++ lib/nishos_reflection.lua
@@ -1,5 +1,8 @@
--- clocked pattern recorder library @alanza and @dan_derks
--- modified for nisho @sonocircuit
+--- clocked pattern recorder library
+--
+-- @module lib.reflection
+-- @author rylee & dan & sacha
+-- modified @sonocircuit for nisho
 
 local reflection = {}
 reflection.__index = reflection
@@ -8,35 +11,36 @@ reflection.__index = reflection
 function reflection.new(id)
   local p = {}
   setmetatable(p, reflection)
-  p.id = id or 1
-  p.rec = 0
-  p.rec_enabled = 0
-  p.play  = 0
-  p.event = {}
-  p.event_prev = {}
-  p.step  = 0
-  p.count = 0
-  p.loop = 0
-  p.clock = nil
-  p.queued_rec  = nil
-  p.rec_dur = nil
-  p.quantize = 1/32
-  p.endpoint = 0
-  p.endpoint_init = 0
-  p.step_min = 0
-  p.step_max = 0
-  p.position = 1
-  p.manual_length = false
-  p.start_callback = function() end
-  p.step_callback = function() end
+  p.id                   = id or 1
+  p.rec                  = 0
+  p.rec_enabled          = 0
+  p.play                 = 0
+  p.event                = {}
+  p.event_prev           = {}
+  p.step                 = 0
+  p.count                = 0
+  p.loop                 = 0
+  p.clock                = nil
+  p.queued_rec           = nil
+  p.rec_dur              = nil
+  p.quantize             = 1 / 32
+  p.endpoint             = 0
+  p.endpoint_init        = 0
+  p.step_min             = 0
+  p.step_max             = 0
+  p.position             = 1
+  p.manual_length        = false
+  p.start_callback       = function() end
+  p.step_callback        = function() end
   p.end_of_loop_callback = function() end
-  p.end_of_rec_callback = function() end
-  p.end_callback = function() end
-  p.process = function(_) end
+  p.start_rec_callback   = function() end
+  p.end_of_rec_callback  = function() end
+  p.end_callback         = function() end
+  p.process              = function(_) end
   return p
 end
 
-local PPQN = 64 -- clock resolution in ppqn
+local PPQN = 64 -- clock resolution
 
 local function deep_copy(tbl)
   local ret = {}
@@ -65,34 +69,35 @@ end
 
 --- start transport
 function reflection:start(beat_sync)
-  beat_sync = beat_sync or self.quantize
+  local beat_sync = beat_sync or self.quantize
   if self.clock then
     clock.cancel(self.clock)
   end
   self.clock = clock.run(function()
-    clock.sync(beat_sync)
+    clock.sync(beat_sync, offset)
     self:begin_playback()
   end)
 end
 
 --- stop transport
-function reflection:stop()
+function reflection:stop(beat_sync)
+  local beat_sync = beat_sync or self.quantize
   if self.clock then
     clock.cancel(self.clock)
   end
   self.clock = clock.run(function()
-    clock.sync(self.quantize)
+    clock.sync(beat_sync)
     self:end_playback()
   end)
 end
 
 --- enable / disable record head
--- rec 1 for recording, 2 for queued recording or 0 for not recording
--- dur (optional) duration in beats for recording
--- beat_sync (optional) sync recording start to beat value
 function reflection:set_rec(rec, dur, beat_sync)
   self.rec = rec == 1 and 1 or 0
   self.rec_enabled = rec > 0 and 1 or 0
+  if self.rec == 1 then
+    self.start_rec_callback()
+  end
   if rec == 1 and self.play == 0 then
     self:start(beat_sync)
   end
@@ -101,7 +106,7 @@ function reflection:set_rec(rec, dur, beat_sync)
     self.event_prev = deep_copy(self.event)
   end
   if rec == 1 and dur then
-    self.rec_dur = {count = dur, length = dur}
+    self.rec_dur = { count = dur, length = dur }
   end
   if rec == 2 then
     if self.count > 0 then
@@ -112,13 +117,13 @@ function reflection:set_rec(rec, dur, beat_sync)
         self.start_callback = fn
       end
     else
-      self.queued_rec = {queued = true, active = true, duration = dur}
+      self.queued_rec = { state = true, duration = dur }
     end
   end
   if rec == 0 then
-    self.queued_rec = nil
     self:_clear_flags()
     self.end_of_rec_callback()
+    self.queued_rec = nil
   end
 end
 
@@ -129,10 +134,10 @@ end
 
 --- quantize playback
 function reflection:set_quantization(q)
-  self.quantize = q == nil and 1/48 or q
+  self.quantize = q == nil and 1 / 32 or q
 end
 
---- set length in beats
+--- change pattern length in beats
 function reflection:set_length(beats)
   if self.count > 0 then
     self.endpoint = beats * PPQN
@@ -140,14 +145,14 @@ function reflection:set_length(beats)
   end
 end
 
--- if temp event table contains data then replace event table
+--- undo previous overdub
 function reflection:undo()
   if next(self.event_prev) then
     self.event = deep_copy(self.event_prev)
   end
 end
 
---- reset pattern
+--- reset
 function reflection:clear()
   if self.clock then
     clock.cancel(self.clock)
@@ -171,20 +176,19 @@ end
 --- watch
 function reflection:watch(event)
   local step_one = false
-  local offset = 1
+  local offset = 0
   if self.queued_rec ~= nil then
-    if self.queued_rec.queued then
-      self:set_rec(1, self.queued_rec.duration, 1/64)
-      self.queued_rec.queued = false
-    end
-    if self.queued_rec.active then
+    if self.queued_rec.state then
+      self:set_rec(1, self.queued_rec.duration, 1/PPQN)
+      self.queued_rec.state = false
       step_one = true
     end
-    offset = 2
+    offset = 1
   end
   if (self.rec == 1 and self.play == 1) or step_one then
     event._flag = true
     local s = step_one and 1 or math.floor(self.step + offset)
+    if s == 0 then s = 1 end
     if not self.event[s] then
       self.event[s] = {}
     end
@@ -193,30 +197,55 @@ function reflection:watch(event)
   end
 end
 
+--- insert event
+function reflection:insert(event, step)
+  if step then
+    if step <= self.endpoint then
+      if not self.event[step] then
+        self.event[step] = {}
+      end
+      table.insert(self.event[step], event)
+      self.count = self.count + 1
+    end
+  end
+end
+
+-- must be called from within a clock.run
 function reflection:begin_playback()
   self.step = self.step_min
   self.play = 1
   self.start_callback()
-  if self.queued_rec ~= nil then
-    self.queued_rec.active = false
-  end
+  local queued_start_callback = false
+  local queued_end_of_loop_callback = false
+  local queued_end_playback = false
   while self.play == 1 do
-    clock.sync(1/PPQN)
+    if queued_start_callback then
+      self.start_callback()
+      queued_start_callback = false
+    end
+    if queued_end_of_loop_callback then
+      self.end_of_loop_callback()
+      queued_end_of_loop_callback = false
+    end
+    if queued_end_playback then
+      self:end_playback()
+      queued_end_playback = false
+      break
+    end
     self.step = self.step + 1
     local q = math.floor(PPQN * self.quantize)
     if self.endpoint == 0 then
       -- don't process on first pass
       if self.rec_dur then
-        self.rec_dur.count = self.rec_dur.count - 1/PPQN
-        if self.rec_dur.count <= 0 then
+        self.rec_dur.count = self.rec_dur.count - 1 / PPQN
+        if self.rec_dur.count <= (self.queued_rec ~= nil and 1/PPQN or 0) then
           self.endpoint = self.rec_dur.length * PPQN
           self.endpoint_init = self.endpoint
           self.step_max = self.endpoint
           self:set_rec(0)
           self.rec_dur = nil
-          -- if loop then start from beginning
           if self.loop == 1 then
-            self.start_callback()
+            queued_start_callback = true
             self.step = self.step_min
             self.play = 1
           end
@@ -227,56 +256,58 @@ function reflection:begin_playback()
           self.endpoint_init = self.step
           self.manual_length = true
           self.step_max = self.step
-          if self.loop == 1 then 
+          if self.loop == 1 then
             self.step = self.step_min
             self:_clear_flags()
-            self:start_callback()
+            queued_start_callback = true
           end
         end
       end
-    -- if not first pass then do the quantization math
     else
-      self:step_callback()
+      -- not first pass, can now process
+      self.step_callback()
       if self.step % q ~= 1 then goto continue end
-      for i = q - 1, 0, - 1 do
+      for i = q - 1, 0, -1 do
         if self.event[self.step - i] and next(self.event[self.step - i]) then
-          for j = 1, #self.event[self.step - i] do
-            local event = self.event[self.step - i][j]
-            if not event._flag then self.process(event, self.id) end
+          for _, event in ipairs(self.event[self.step - i]) do
+            if not event._flag then
+              self.process(event, self.id)
+            end
           end
         end
       end
       ::continue::
-      -- if overdubbing with dur as arg then cound down and end rec
       if self.rec_dur then
-        self.rec_dur.count = self.rec_dur.count - 1/PPQN
+        self.rec_dur.count = self.rec_dur.count - 1 / PPQN
         if self.rec_dur.count <= 0 then
           self:set_rec(0)
           self.rec_dur = nil
         end
       end
-      -- if the endpoint is reached restart or stop playback
+      -- if the endpoint is reached reset counter or stop playback
       if self.count > 0 and self.step >= self.step_max then
-        self.end_of_loop_callback()
+        queued_end_of_loop_callback = true
         if self.loop == 0 then
-          self:end_playback()
+          queued_end_playback = true
         elseif self.loop == 1 then
           self.step = self.step_min
+          queued_start_callback = true
           self:_clear_flags()
-          self:start_callback()
         end
       end
     end
+    clock.sync(1 / PPQN)
   end
 end
 
-function reflection:end_playback()
-  if self.clock then
+function reflection:end_playback(silent)
+  if self.clock and not silent then
     clock.cancel(self.clock)
+    self.clock = nil
   end
   self.play = 0
   self.rec = 0
-  -- if first pass then set the endpoint if events were recorded
+  self.rec_enabled = 0
   if self.endpoint == 0 and next(self.event) then
     self.endpoint = self.step
     self.step_max = self.endpoint
